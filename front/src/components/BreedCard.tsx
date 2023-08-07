947fecbf9255b459b1e91e0ded797c988ded710a
Ylidox <alievartiom2017@gmail.com>
2023-08-07 13:59:27 +0300

    update breedcard

--- front/src/components/BreedCard.tsx
+++ front/src/components/BreedCard.tsx
@@ -6,6 +6,20 @@ import {motion, AnimatePresence} from 'framer-motion';
 export const BreedCard: FC<IBreed> = ({id, name, origin, description}) => {
   let [pictures, setPictures] = useState<ICat[]>([]);
   let [showModal, setShowModal] = useState<boolean>(false);
+  let [showDescription, setShowDescription] = useState<boolean>(false);
+  let [index, setIndex] = useState<number>(0);
+
+  let incrementIndex = () => {
+    index++;
+    index %= pictures.length;
+    setIndex(index);
+  }
+
+  let decrementIndex = () => {
+    index--;
+    if(index < 0) index = pictures.length - 1;
+    setIndex(index);
+  }
   
   let getPicture = async () => {
     let res = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`);
@@ -14,9 +28,20 @@ export const BreedCard: FC<IBreed> = ({id, name, origin, description}) => {
     setPictures([...pictures, ...ans]);
   }
 
+  let getPictures = async () => {
+    let res = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}&limit=10`);
+    let ans: ICat[] = await res.json();
+
+    setPictures([...pictures, ...ans]);
+  }
+
   useEffect(() => {
     if(!pictures.length) getPicture();
   }, []);
+
+  useEffect(() => {
+    if(showModal && pictures.length < 11) getPictures();
+  }, [showModal]);
 
   return (
     <AnimatePresence>
@@ -32,11 +57,42 @@ export const BreedCard: FC<IBreed> = ({id, name, origin, description}) => {
               e.stopPropagation();
             }}
           >
-            <div className={styles.content}>
+            <motion.div className={styles.content}>
               {(!!pictures.length) &&
-                <img src={pictures[0].url} className={styles.img}/>
+                <img src={pictures[index].url} className={styles.img}/>
               }
-            </div>
+              <div className={styles.button_container}>
+                <div className={styles.button_left}
+                  onClick={() => decrementIndex()}
+                ></div>
+                <div className={styles.button_right}
+                  onClick={() => decrementIndex()}
+                ></div>
+              </div>
+
+              <div className={styles.info_container}
+                onClick={() => {
+                  setShowDescription(!showDescription);
+                }}
+              >
+                <div className={styles.info_header}>
+                  <h2>{name}</h2>
+                  <div className={styles.origin}>
+                    {origin}
+                  </div>
+                </div>
+                <AnimatePresence>
+                  {showDescription && 
+                    <motion.div className={styles.description}
+                      initial={{height:0}}
+                      animate={{height:'auto'}}
+                      exit={{height:0}}
+                    >
+                      {description}
+                    </motion.div>}
+                </AnimatePresence>
+              </div>
+            </motion.div>
           </motion.div> 
         </div>
         :
@@ -48,7 +104,7 @@ export const BreedCard: FC<IBreed> = ({id, name, origin, description}) => {
         >
           <div className={styles.content}>
             {(!!pictures.length) &&
-              <img src={pictures[0].url} className={styles.img}/>
+              <img src={pictures[index].url} className={styles.img}/>
             }
           </div>
         </motion.div>
